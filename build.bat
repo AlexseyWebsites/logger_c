8bb28cef453626b09a7f87ffdc678b5540e89705
Alexsey <alexsey.websites.work@gmail.com>
2025-11-14 21:39:29 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,9 +1,14 @@
 @echo off
 setlocal
 
-rem Compilation of source files
-gcc -c src/logger.c -o logger.o -std=c11
-gcc test/test_logger.c logger.o -o test_logger.exe -lws2_32 -lmsvcrt -std=c11
+rem Creating directories if there are none
+if not exist "build" mkdir build
+if not exist "logs" mkdir logs
 
-echo The assembly is completed
-pause
+rem Compilation of source files
+gcc -c src/logger.c -o build/logger.o -std=c11 -Isrc
+gcc test/test_logger.c build/logger.o -o build/test_logger.exe -lws2_32 -lmsvcrt -std=c11
+
+echo The build is complete!
+echo Executable files in the build
+pause folder
